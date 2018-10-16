64cd1dc4857905a481285179745863626215db3f
ravitejakv <43729349+ravitejakv@users.noreply.github.com>
2018-10-16 19:14:43 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":4,"title":"","author":"","fileName":"ICC17_LAAA_0116AO_Blank_Final.pdf","bounds":[[935,1210],[935,1210],[935,1210],[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
---
+++ index.js
@@ -0,0 +1,1 @@
+var mainText = document.getElementById("mainText");
